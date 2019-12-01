3ff3c15214389dacb111e01643e5e01ac1317b4d
Sebastian Stasiak <12955338+stasiaks@users.noreply.github.com>
2019-12-01 18:00:27 +0100

    Slight beautify

--- src/Day1/Day1.hs
+++ src/Day1/Day1.hs
@@ -9,18 +9,19 @@ main = do
   putStrLn "Part 2:"
   putStrLn $ show $ part2 $ input
 
+part1 :: [Integer] -> Integer
 part1 xs = sum $ map calculateFuel xs
 
---part2 xs = cfTR (calculateFuel $ part1 $ xs) (part1 $ xs)
+part2 :: [Integer] -> Integer
 part2 xs = sum $ map (accountForFuel . calculateFuel) xs
 
 calculateFuel :: Integer -> Integer
-calculateFuel a = (div a 3)- 2
+calculateFuel a = (div a 3) - 2
 
 accountForFuel :: Integer -> Integer
-accountForFuel a = cfTR (calculateFuel a) a
+accountForFuel a = accountForFuelTR (calculateFuel a) a
 
-cfTR :: Integer -> Integer -> Integer
-cfTR a b 
-    | a >= 0 = cfTR (calculateFuel a) (a + b)
+accountForFuelTR :: Integer -> Integer -> Integer
+accountForFuelTR a b 
+    | a >= 0 = accountForFuelTR (calculateFuel a) (a + b)
     | otherwise = b

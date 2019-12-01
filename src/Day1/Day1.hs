5c3df9f1ed68f8148ead101e1fe86e99dd73070d
Sebastian Stasiak <arionw@live.com>
2019-12-01 17:57:27 +0100

    Part 2

--- src/Day1/Day1.hs
+++ src/Day1/Day1.hs
@@ -3,6 +3,24 @@ module Main where
 main :: IO ()
 main = do
   contents <- getContents
-  putStr $ show $ sum $ map (calculateFuel . read) (lines contents)
+  let input = map read $ lines contents
+  putStrLn "Part 1:"
+  putStrLn $ show $ part1 $ input
+  putStrLn "Part 2:"
+  putStrLn $ show $ part2 $ input
 
-calculateFuel a = floor (a / 3) - 2
+part1 xs = sum $ map calculateFuel xs
+
+--part2 xs = cfTR (calculateFuel $ part1 $ xs) (part1 $ xs)
+part2 xs = sum $ map (accountForFuel . calculateFuel) xs
+
+calculateFuel :: Integer -> Integer
+calculateFuel a = (div a 3)- 2
+
+accountForFuel :: Integer -> Integer
+accountForFuel a = cfTR (calculateFuel a) a
+
+cfTR :: Integer -> Integer -> Integer
+cfTR a b 
+    | a >= 0 = cfTR (calculateFuel a) (a + b)
+    | otherwise = b

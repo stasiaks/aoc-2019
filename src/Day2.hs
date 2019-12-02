89a97f6a960eecebcefc3b9fd928bdef387e9ac0
Sebastian Stasiak <12955338+stasiaks@users.noreply.github.com>
2019-12-02 23:57:52 +0100

    Some improvements

--- src/Day2.hs
+++ src/Day2.hs
@@ -2,32 +2,21 @@ module Main where
 import Data.List.Split
 import Data.Array.ST
 
-data Opcode = Addition
-            | Multiplication
-            | Halt
-  deriving Show
-
 main :: IO ()
 main = do
   contents <- getContents
   let input = map read (splitOn "," contents) :: [Int]
-  putStrLn $ show $ input
   putStrLn "Part 1:"
   putStrLn $ show $ part1 $ input
 
-part1 xs = exec xs (opcode $ value xs 0) 0
+part1 xs = exec xs 0
 
-exec :: [Int] -> Maybe Opcode -> Int -> Maybe [Int]
-exec xs (Just Multiplication) n = exec (multiplication xs (n+1)) (opcode $ value xs (n+4)) (n+4)
-exec xs (Just Addition)       n = exec (addition xs (n+1))       (opcode $ value xs (n+4)) (n+4)
-exec xs (Just Halt)           _ = Just xs
-exec _ Nothing                _ = Nothing
-
-opcode :: Int -> Maybe Opcode
-opcode 1  = Just Addition
-opcode 2  = Just Multiplication
-opcode 99 = Just Halt
-opcode _  = Nothing
+exec :: [Int] -> Int -> Either String [Int]
+exec xs n
+  | (value xs n) == 1  = exec (addition xs (n+1)) (n+4)       -- Addition
+  | (value xs n) == 2  = exec (multiplication xs (n+1)) (n+4) -- Mutliplication
+  | (value xs n) == 99 = Right xs                             -- Halt
+  | otherwise          = Left "UNEXCPECTED OPCODE"
 
 referenceValue :: [Int] -> Int -> Int
 referenceValue xs n = value xs $ value xs n

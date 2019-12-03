c5b0153824999744da0d28270372ac47394a72c7
Sebastian Stasiak <12955338+stasiaks@users.noreply.github.com>
2019-12-03 20:54:10 +0100

    Reading input

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,6 +1,35 @@
 module Main where
+import Data.List.Split
+import Data.Tuple.Strict
+import Data.Char
+
+data Direction = ToLeft | ToRight | Up | Down
+  deriving Show
+data Movement = Movement Direction Int
+  deriving Show
+
+instance Read Direction where
+  readsPrec _ input = case splitAt 1 input of
+    ("L", rest) -> [(ToLeft, rest)]
+    ("R", rest) -> [(ToRight, rest)]
+    ("U", rest) -> [(Up, rest)]
+    ("D", rest) -> [(Down, rest)]
+    otherwise -> []
+
+instance Read Movement where
+  readsPrec _ input = case mapSnd (span isDigit) $ splitAt 1 input of
+    (dir, (digits, rest)) -> [(Movement (read dir) (read digits), rest)]
 
 main :: IO ()
 main = do
   contents <- getContents
+  let input = tuplify $ map ((map (read :: String -> Movement)) . splitOn ",") (lines contents)
   putStrLn "Part 1:"
+  putStrLn $ show input
+
+part1 :: ([Movement], [Movement]) -> Int
+part1 (xs, ys) = 1
+
+tuplify :: [a] -> (a,a)
+tuplify [x,y] = (x,y)
+

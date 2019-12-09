c20db1322a3279908aabd32d446fa2915820ec2a
Sebastian Stasiak <arionw@live.com>
2019-12-09 22:12:05 +0100

    Part 2

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,5 +1,6 @@
 module Main where
 import Data.Digits
+import Data.List
 import Data.List.Split
 
 main :: IO ()
@@ -8,12 +9,20 @@ main = do
   let [inputFrom, inputTo] = map read $ splitOn "-" contents
   putStrLn "Part 1:"
   putStrLn $ show $ part1 inputFrom inputTo
+  putStrLn "Part 2:"
+  putStrLn $ show $ part2 inputFrom inputTo
 
 part1 :: Int -> Int -> Int
 part1 a b = length $ filter (\x -> neverDecreases x && twoAdjacentSame x) $ map (digits 10) [a..b]
+
+part2 :: Int -> Int -> Int
+part2 a b = length $ filter (\x -> neverDecreases x && onlyTwoAdjacentSame x) $ map (digits 10) [a..b]
 
 neverDecreases :: Ord a => [a] -> Bool
 neverDecreases xs = all (==True) $ zipWith (\x y -> x <= y) xs (tail xs)
 
 twoAdjacentSame :: Eq a => [a] -> Bool
-twoAdjacentSame xs = any (==True ) $ zipWith (\x y -> x == y) xs (tail xs)
+twoAdjacentSame xs = any (\x -> length x >= 2) $ group xs
+
+onlyTwoAdjacentSame :: Eq a => [a] -> Bool
+onlyTwoAdjacentSame xs = any (\x -> length x == 2) $ group xs

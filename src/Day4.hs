f82016730c610254364541552e75ebcbac0f5bf6
Sebastian Stasiak <git@stasiaks.com>
2025-11-23 02:32:02 +0100

    Part 1

--- src/Day4.hs
+++ src/Day4.hs
@@ -10,4 +10,10 @@ main = do
   putStrLn $ show $ part1 inputFrom inputTo
 
 part1 :: Int -> Int -> Int
-part1 a b = 0
+part1 a b = length $ filter (\x -> neverDecreases x && twoAdjacentSame x) $ map (digits 10) [a..b]
+
+neverDecreases :: Ord a => [a] -> Bool
+neverDecreases xs = all (==True) $ zipWith (\x y -> x <= y) xs (tail xs)
+
+twoAdjacentSame :: Eq a => [a] -> Bool
+twoAdjacentSame xs = any (==True ) $ zipWith (\x y -> x == y) xs (tail xs)
